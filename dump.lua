92e7cf4fcc45b61073388d67d31f70968ed1b4db
Pavel S <pa_s@mail.ru>
2011-01-23 22:33:37 +0100

    fixes dump function

--- dump.lua
+++ dump.lua
@@ -2,7 +2,14 @@ function indent(lvl)
   return string.rep ("  ", lvl)
 end
 
-function tagdump(obj, depth, lvl, unfold, fp)
+local rrr = {}
+
+for i=1,8 do
+  rrr[i] = string.format("[1;%dm", 30+i-1)
+end
+
+
+function tagdump(obj,id,depth, lvl, unfold, fp)
   -- [[
   --if not dump_subtree(obj) then
   local green = "[1;32m"
@@ -10,29 +17,21 @@ function tagdump(obj, depth, lvl, unfold, fp)
   local red = "[1;31m"
   local b = "[1;34m"
   local clear = "[0;m"
+  
     do
-      fp:write(indent(lvl) .. green .. tostring(obj["tag"]) .. clear .. " = "..b.."{"..clear.."\n")
+      fp:write(indent(lvl) .. id .. ":" .. rrr[lvl%7+1] .. tostring(obj["tag"]) .. clear .. " = "..b.."{"..clear.."\n")
       for k,v in pairs(obj) do
         if k ~= "tag" and string.byte(k,1) ~= 95 then
           if type(v) == "table" then
             if v.tag and (not unfold) then
-              if v.tag == "token" then
-                if k == "identifier" then
-                  fp:write(yellow)
-                  dump(v.value,depth,lvl+1, unfold, fp)
-                  fp:write(clear)
-                else
-                  fp:write(indent(lvl+1) ..  red .. tostring(k) .. clear .. "\n")
-                end
-              else
-                tagdump(v,depth,lvl+1,unfold, fp)
-              end
+              tagdump(v,k,depth,lvl+1,unfold, fp)
             else
-              fp:write(indent(lvl) .. tostring(k) .. " = \n")
-              dump(v, depth, lvl+1, unfold, fp)
+              fp:write(indent(lvl+1) .. tostring(k) .. " = {\n")
+              _dump(v, depth, lvl+2, unfold, fp)
+              fp:write(indent(lvl+1) .. "}\n")
             end
           else
-            fp:write(indent(lvl+1) .. tostring(k) .. " = [[" .. tostring(v) .. "]]\n")
+            fp:write(indent(lvl+1) .. tostring(k) .. " = >" .. tostring(v) .. "<\n")
           end
         end
       end
@@ -42,13 +41,10 @@ function tagdump(obj, depth, lvl, unfold, fp)
   fp:flush()
 end
 
-function dump(obj, depth, lvl, unfold, fp)
-  if not lvl then lvl = 1 end
-  if not fp then fp = io.stdout end
-  
+function _dump(obj, depth, lvl, unfold, fp)
   if type(obj) == "table" then
     if obj.tag then
-      tagdump(obj,depth,lvl,unfold, fp)
+      tagdump(obj,"root",depth,lvl,unfold, fp)
     else
       if lvl == 1 then
         fp:write("{\n")
@@ -60,16 +56,16 @@ function dump(obj, depth, lvl, unfold, fp)
             fp:write(indent(lvl) .. tostring(k) .. " table\n")
           else
             if v.tag and (not unfold) then
-              tagdump(v,depth,lvl+1, unfold, fp)
+              tagdump(v,k,depth,lvl, unfold, fp)
             else
               if type(k) == "number" then
                 fp:write(indent(lvl) .. string.format("%d", k).. " = {\n")
               else
                 fp:write(indent(lvl) .. tostring(k) .. " = {\n")
               end
-              dump(v,depth, lvl+1, unfold, fp)
+              _dump(v,depth, lvl+1, unfold, fp)
+              fp:write(indent(lvl) .. "}\n")
             end
-            fp:write(indent(lvl) .. "}\n")
           end
         else
           fp:write(indent(lvl) .. tostring(k) .. " = " .. tostring(v) .. "\n")
@@ -87,3 +83,12 @@ function dump(obj, depth, lvl, unfold, fp)
   return obj
 end
 
+function dump(obj, depth, lvl, unfold, fp)
+  if not lvl then lvl = 1 end
+  if not fp then fp = io.stdout end 
+  
+  fp:write(">>>>>>>>>>>>>>>>>>>>>\n")
+  _dump(obj, depth, lvl, unfold, fp)
+  fp:write("<<<<<<<<<<<<<<<<<<<<<\n")
+  fp:flush()
+end
